4e0b60ea958b5c62da9c3df4f74c8d825e7aaee2
katheee1 <carol.ramirez-c@uniminuto.edu.co>
2026-03-02 08:21:55 -0500

    Actualización de productos y configuración

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React, {useState} from "react";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./components/layout/MainLayout";
 
@@ -6,14 +8,34 @@ import Products from "./screens/Products";
 import Contact from "./screens/Contact";
 
 export default function App() {
+  // carrito sea global
+  const [cart, setCart] = useState([]);
+  // estado para la notificacion
+  const showToast = (text) => {
+    setToast(text);
+    setTimeout(()=> setToast(""), 2000); // se oculta despues de 2 segundos
+  };
+
+  // funcion ara agregar productos al carrito
+  const handleAddToCart =(product) =>{
+    setCart([...cart, product]);
+    showToast("Producto agregado al carrito");
+
+  };
+
+
   return (
     <BrowserRouter>
-      <MainLayout>
+      <MainLayout cartCount={cart.length} >
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
+
+          <Route path="/products" element={<Products onAddToCart={handleAddToCart}  />} />
+
           <Route path="/contact" element={<Contact />} />
         </Routes>
+        {/* notificacion visible solo cuando toast tiene texto*/}
+        {toast && <Toast message={toast} />}
       </MainLayout>
     </BrowserRouter>
   );
